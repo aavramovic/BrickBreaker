004b26dad07f479af9a9741ccffda22a1cb38ae5
TRKing <antonio.avramovic@gmail.com>
2019-06-22 17:34:26 +0200

    Antonio start

--- BB/BrickBreaker/BrickBreaker/Ball.cs
+++ BB/BrickBreaker/BrickBreaker/Ball.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace BrickBreaker
 {
     class Ball
     {
+        public int Radius { get; set; }
+        public Point Position { get; set; }
+        public Color Coloring { get; set; }
+        //Optional
+        public double Speed { get; set; }
+        
+        public Ball()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
     }
 }
--- BB/BrickBreaker/BrickBreaker/Bouncer.cs
+++ BB/BrickBreaker/BrickBreaker/Bouncer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace BrickBreaker
 {
     class Bouncer
     {
+        public float Width { get; set; }
+        public float Length { get; set; }
+        public Point Position { get; set; }
+        public Color Coloring { get; set; }
+        public double Speed { get; set; }
     }
 }
