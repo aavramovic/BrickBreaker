16ec5948bb1ebe6030ab3909109693e510b6c511
Evgenij Kostadinov <ekostadinov5@gmail.com>
2019-06-24 02:22:59 +0200

    Start main menu

--- BB/BrickBreaker/BrickBreaker/Form1.Designer.cs
+++ BB/BrickBreaker/BrickBreaker/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(540, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 53);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(549, 118);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(48, 53);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(540, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 53);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Level:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(549, 398);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(41, 53);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(900, 550);
+            this.MinimumSize = new System.Drawing.Size(900, 550);
+            this.Name = "Form1";
+            this.Text = "Bricks Breaker";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- BB/BrickBreaker/BrickBreaker/Form1.cs
+++ BB/BrickBreaker/BrickBreaker/Form1.cs
@@ -10,11 +10,115 @@ using System.Windows.Forms;
 
 namespace BrickBreaker
 {
+    enum Mode
+    {
+        MAIN_MENU,
+        SELECT_LEVEL,
+        PLAY
+    }
     public partial class Form1 : Form
     {
+        private Game game;
+        private Mode mode;  
+        private string fileName;
+        private List<Control> controls;
+
         public Form1()
         {
             InitializeComponent();
+            DoubleBuffered = true;
+            controls = new List<Control>();
+
+            DrawMainMenu();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        /**
+         *  Го креира главното мени преку бришење на предходно прикажаните контроли 
+         *  и динамичко креирање и додавање на соодветни нови контроли на формата.
+         */
+        private void DrawMainMenu()
+        {
+            this.Controls.Clear();
+
+            Button buttonPlay = CreateButton(80, 45, 225, 55, "Play");
+            buttonPlay.Click += new EventHandler(BtnPlay_Click);
+            controls.Add(buttonPlay);
+
+            Button buttonSaveGame = CreateButton(125, 115, 225, 55, "Save Game");
+            buttonSaveGame.Click += new EventHandler(BtnSaveGame_Click);
+            controls.Add(buttonSaveGame);
+
+            Button buttonLoadGame = CreateButton(80, 185, 225, 55, "Load Game");
+            buttonLoadGame.Click += new EventHandler(BtnLoadGame_Click);
+            controls.Add(buttonLoadGame);
+
+            Button buttonOptions = CreateButton(125, 255, 225, 55, "Options");
+            buttonOptions.Click += new EventHandler(BtnOptions_Click);
+            controls.Add(buttonOptions);
+
+            Button buttonQuit = CreateButton(80, 325, 225, 55, "Quit");
+            buttonQuit.Click += new EventHandler(BtnQuit_Click);
+            controls.Add(buttonQuit);
+
+            controls.ForEach(c => this.Controls.Add(c));
+
+            mode = Mode.MAIN_MENU;
+        }
+
+        private void DrawSelectLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        /**
+         *  Креира контрола-копче со зададени почетни x и y координати, ширина, висина и текст кој треба да се прикаже на копчето.
+         */
+        private Button CreateButton(int x, int y, int width, int height, string text)
+        {
+            Button newButton = new Button();
+            newButton.Location = new Point(x, y);
+            newButton.Size = new Size(width, height);
+            newButton.BackColor = Color.DimGray;
+            newButton.ForeColor = Color.White;
+            newButton.Font = new Font("Showcard Gothic", 14, FontStyle.Regular);
+            newButton.Text = text;
+            newButton.Name = "btn" + text.Replace(" ", "");
+            return newButton;
+        }
+
+        private Label CreateLabel()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("Play");
+        }
+
+        private void BtnSaveGame_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("SG");
+        }
+
+        private void BtnLoadGame_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("LG");
+        }
+
+        private void BtnOptions_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException("OPTS");
+        }
+
+        private void BtnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
